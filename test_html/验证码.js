9fccb3e2f2873bbe19d5555841a0d958dd173924
saber <2373632346@qq.com>
2019-05-22 17:04:16 +0800

    测试验证码

---
+++ test_html/验证码.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+<canvas id="canvas" width="120" height="40"></canvas>
+  <a href="#" id="changeImg">看不清，换一张</a>
+  <script src="验证码.js"></script>
+
+</body>
+</html>
